2467bddd921e546411000d02e580acae3425937e
nnaarrriinn <attnarin@gmail.com>
2024-05-21 16:21:29 +0900

    main

---
+++ game/game1.java
@@ -0,0 +1,12 @@
+package game;
+
+public class game1 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("이석민");
+		sb.append("최승철");
+		System.out.println(sb);
+	}
+
+}
